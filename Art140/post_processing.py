e056c23205a894411e12aeb140079178e8126d6b
Fakesmile <fengmao2@illinois.edu>
2019-11-17 20:11:54 -0600

    modifying for Art

--- Art140/post_processing.py
+++ Art140/post_processing.py
@@ -7,18 +7,20 @@ import RPi.GPIO as GPIO
 import time
 import GPIO_IR
 import random
-
+doorlock =False
 door = ['door','lock']
-
 unlock = ['unlock','unlocked']
 
+light_livingroom =False
 livingroom = ['livingroom', 'living'] 
 
+light_study =False
 study = ['study']
 
+light_bathroom =False
 shower = ['bathroom', 'shower']
 
-night =['night','sleep']
+night =['night','sleep', 'off']
 
 plant = ['plant', 'watering','water', 'plants']
 
@@ -28,59 +30,103 @@ trash =['recycle','bin','trash','waste']
 
 thanks = ['thanks','thank']
 
+speaker = False
 music = ['music']
 
 def lock_door():
     global doorlock
-    if(!doorlock)
-        return "your door is unlocked, locking it for you.";
+    if not doorlock:
+        doorlock = True
+        return "your door is unlocked, locking it for you."
     else:
-        return "your door is securely locked.";
+        doorlock = False
+        return "your door is securely locked."
     
 def unlock_door():
     global doorlock
-    if(doorlock)
-        return "welcome home, unlocking the door for you.";
+    if doorlock:
+        doorlock = False
+        return "welcome home, unlocking the door for you."
     else:
-        return "your door is not locked. Wanna lock it?";
+        doorlock = True
+        return "your door is not locked. Wanna lock it?"
 
-def livingroom():
+def inLivingroom():
     global light_livingroom
     global light_livingroom_pin
-    if(light_livingroom)
-        
+    if light_livingroom:
+        light_livingroom = False
         return "turning living room light off. Good night."
     else:
-        
+        light_livingroom = True
         return "turning living room light on. want some music?"
     
-def study():
+def inStudy():
     global light_study
     global light_study_pin
-    if(light_study)
-        
+    if light_study:
+        light_study = False
         return "turning study light off. Good work today."
     else:
-        
+        light_study = True
         return "turning study light on. want some music?"
     
-def bathroom():
+def inBathroom():
     global light_bathroom
     global light_bathroom_pin
-    if(light_bathroom)
-        
+    if light_bathroom:
+        light_bathroom = False
         return "turning living room light off. Good night."
     else:
-        
+        light_bathroom = True
         return "turning living room light on. want some music?"
 
-def throw_trash():
-    ran = random.randint()
+def goodNight():
+    ran = random.randint(1,10)
     if ran%5 ==0:
-        return "your door is unlocked.";
+        return "We are gonna be up all night."
     else:
-        return "your door is securely locked.";
-sensors = {'humidity':(humidity, get_humidity), 'temperature':(temperature, get_temperature), 'barcode':(barcode, get_barcode), 'ir':(door, get_door)}
+        return "Good Night."
+
+def waterPlants():
+    ran = random.randint(1,10)
+    if ran%5 ==0:
+        return "Opps your plant is dead."
+    else:
+        return "Watering plants."
+
+def catPlay():
+    ran = random.randint(1,10)
+    if ran%5 ==0:
+        return "Hiss Hiss."
+    else:
+        return "Meow Meow."
+
+def throwTrash():
+    ran = random.randint(1,10)
+    if ran%5 ==0:
+        return "your door is unlocked."
+    else:
+        return "your door is securely locked."
+
+def thankYou():
+    ran = random.randint(1,10)
+    if ran%5 ==0:
+        return "your door is unlocked."
+    else:
+        return "your door is securely locked."
+
+def playMusic():
+    global speaker
+    if speaker:
+        speaker =False
+        return "Music Off."
+    else:
+        speaker = True
+        return "Music On."
+
+sensors = {'lock':(door, lock_door), 'unlock':(unlock, unlock_door), 'livingroom':(livingroom, inLivingroom), 'study':(study, inStudy), 'bathroom':(bathroom,inBathroom),
+'night':(night,goodNight),'plants':(plant,waterPlants),'cat':(cat,catPlay),'trash':(trash,throwTrash),'thanks':(thanks,thankYou),'music':(music,playMusic)}
 
 def check_for_keyword(words):
     words = words.split(' ')
